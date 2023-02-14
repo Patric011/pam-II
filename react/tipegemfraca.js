299bf22a0099acb1fe0be58f11d5f5d1a0129268
Patric011 <Pattricsilva11@gmail.com>
2023-02-13 22:37:01 -0300

    Aula 01 JS

---
+++ react/tipegemfraca.js
@@ -0,0 +1,13 @@
+let qualquer = ["Patric", "Silva"]
+let qualquer2 = {
+    nome: "Patric",
+    idade: 23
+}
+console.log(qualquer)
+console.log(Array.isArray(qualquer))
+console.log(qualquer2)
+console.log(typeof qualquer2)
+
+qualquer = 3.14
+console.log (qualquer)
+console.log (typeof qualquer) 
