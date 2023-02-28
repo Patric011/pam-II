534e5ada57663d0ca3037f4fb4030ebe4fb9d053
Patric011 <pattricsilva11@gmail.com>
2023-02-27 22:36:21 -0300

    aula 02

---
+++ react/exercicio01.js
@@ -0,0 +1,8 @@
+function calculo () {
+    let num1 = 10;
+    let num2 = 5;
+    return calculo = num1 + num2;
+    
+    }
+    console.log(calculo());
+    
---
+++ react/funcoes.js
@@ -0,0 +1,20 @@
+
+function mensagem () {
+let msg = "funcao 02";
+return msg
+
+}
+console.log(mensagem())
+
+const nome = ()=> {
+    return "funcao 03";
+}
+console.log(nome())
+
+function lindaMensagem (msg) {
+    return console.log("A sua mensagem é: ", msg)
+}
+
+lindaMensagem("Java é bom! É incrivel!");
+lindaMensagem(3);
+lindaMensagem("Coitado do Juarez por me aguentar nas segundas");
